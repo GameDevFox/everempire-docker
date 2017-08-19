009684c226c6097195a2de147d1030109bdf1f47
Edward Nicholes Jr <GameDevFox@gmail.com>
2017-08-19 11:24:04 -0500

    Initial commit

---
+++ everempire-db/init.sql
@@ -0,0 +1,32 @@
+CREATE TABLE "user" (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  email VARCHAR(256)
+);
+
+CREATE TABLE auth (
+  id SERIAL PRIMARY KEY,
+  provider VARCHAR(16) NOT NULL,
+  provider_uid VARCHAR(32) NOT NULL,
+  user_id INT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES "user"(id),
+  UNIQUE (provider, provider_uid)
+);
+
+CREATE TABLE token (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  token VARCHAR(64) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES "user"(id),
+  UNIQUE (user_id)
+);
+
+CREATE TABLE world (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(32) NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES "user"(id)
+);
+
